48e9fbcce930476f11afc74e1fa714ad0393cf51
N0mekop <blinkevin@hotmail.com>
2023-01-29 19:19:26 +0100

    Installation Assert + Création entité Group

---
+++ src/Entity/Group.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GroupRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: GroupRepository::class)]
+#[UniqueEntity(fields: ['name'])]
+#[ORM\Table(name: '`group`')]
+class Group
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    #[Assert\NotBlank()]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
