23f20d57bccd08b29e09761a7dd24582df02ddd2
N0mekop <blinkevin@hotmail.com>
2023-01-30 11:52:15 +0100

    Création entité UserTeam

--- src/Entity/Team.php
+++ src/Entity/Team.php
@@ -3,9 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\TeamRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TeamRepository::class)]
 #[UniqueEntity(fields: ['name'])]
@@ -17,9 +17,17 @@ class Team
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255, unique: true)]
+    #[ORM\Column(length: 255, unique: true))]
     #[Assert\NotBlank()]
     private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'team', targetEntity: UserTeam::class, orphanRemoval: true)]
+    private Collection $userTeams;
+
+    public function __construct()
+    {
+        $this->userTeams = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -34,6 +42,36 @@ class Team
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, UserTeam>
+     */
+    public function getUserTeams(): Collection
+    {
+        return $this->userTeams;
+    }
+
+    public function addUserTeam(UserTeam $userTeam): self
+    {
+        if (!$this->userTeams->contains($userTeam)) {
+            $this->userTeams->add($userTeam);
+            $userTeam->setTeam($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserTeam(UserTeam $userTeam): self
+    {
+        if ($this->userTeams->removeElement($userTeam)) {
+            // set the owning side to null (unless already changed)
+            if ($userTeam->getTeam() === $this) {
+                $userTeam->setTeam(null);
+            }
+        }
 
         return $this;
     }
